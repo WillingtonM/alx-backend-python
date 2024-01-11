c88525168753a5d0742128b6837a5e7bbf95a004
WillingtonM <willington.mhlanga@gmail.com>
2024-01-11 23:42:12 +0200

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,7 +5,6 @@
 
 def concat(str1: str, str2: str) -> str:
     """Takes 2 str variables & returns their concatenation"""
-    
     if isinstance(str1, str) and isinstance(str2, str):
         return f"{str1}{str2}"
     raise TypeError("Only strings should be passed")
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,7 +5,6 @@
 
 def floor(n: float) -> int:
     """Returns floor of float passed to it"""
-    
     if not isinstance(n, float):
         raise TypeError('An invalid parameter passed, float was expected')
     return int(n)
